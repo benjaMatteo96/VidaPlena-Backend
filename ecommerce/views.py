44c6fac24c0ab818ab3ed070e07aa72037803500
Benjamin Matteo <127891765+benjaMatteo96@users.noreply.github.com>
2024-07-21 22:42:44 -0400

    update serializers

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,3 @@
-# ecommerce/views.py
-
 from rest_framework import viewsets
 from .models import Category, Product, Order, OrderItem
 from .serializers import CategorySerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
@@ -13,8 +11,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-   
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
